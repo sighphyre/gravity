fb59042a6bab2898d65bdf7428e157cf6f8002c4
Simon <liquidwicked64@gmail.com>
2017-05-30 22:41:58 +0200

    made chat app by mistake

--- gravity-api/main.py
+++ gravity-api/main.py
@@ -1,68 +1,153 @@
-###############################################################################
-#
-# The MIT License (MIT)
-#
-# Copyright (c) Crossbar.io Technologies GmbH
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-###############################################################################
+import sys
+import json
+import jwt
 
-from autobahn.twisted.websocket import WebSocketServerProtocol, \
-    WebSocketServerFactory
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.web.server import Site
+from twisted.web.static import File
+
+from autobahn.twisted.websocket import WebSocketServerFactory, \
+    WebSocketServerProtocol, \
+    listenWS
+
+def decode_token(token):
+    # encoded = jwt.encode({'some': 'payload'}, 'secret', algorithm='HS256')
+    try:        
+        return jwt.decode(token, 'secret', algorithms=['HS256'])
+    except(jwt.exceptions.DecodeError):
+        return None    
 
 
-class MyServerProtocol(WebSocketServerProtocol):
+class UserStore:
 
-    def onConnect(self, request):
-        print("Client connecting: {0}".format(request.peer))
+    def __init__(self):
+        self.validatedPeers = []
 
-    def onOpen(self):
-        print("WebSocket connection open.")
+
+store = UserStore()
+
+class BroadcastServerProtocol(WebSocketServerProtocol):
+
+    def __init__(self, **kwargs):
+        WebSocketServerProtocol.__init__(self, **kwargs)
+
+    def onOpen(self):        
+        self.factory.register(self)
 
     def onMessage(self, payload, isBinary):
-        if isBinary:
-            print("Binary message received: {0} bytes".format(len(payload)))
-        else:
-            print("Text message received: {0}".format(payload.decode('utf8')))
+        if not isBinary:            
+            packet = json.loads(payload.decode("utf-8"))
+            if "token" in packet:
+                print("validating token")
+                claims = decode_token(packet["token"])
+                if not claims:
+                    return
+                print(claims)
+                msg = {
+                    "message" : "{} from {}".format(packet["message"], claims["username"]),
+                    "origin": self.peer
+                }
+                # print self.peer
 
-        # echo back message verbatim
-        self.sendMessage(payload, isBinary)
+                self.factory.broadcast(msg)
+            elif "username" in packet and "password" in packet:
+                print "Got user ", packet["username"]
+                credentials = {
+                    "username": packet["username"]
+                }
+                encoded = jwt.encode(credentials, 'secret', algorithm='HS256')
+                message = json.dumps({
+                    "type": "token",
+                    "data": encoded
+                }).encode('utf8')
+                store.validatedPeers.append(self.peer)
+                self.sendMessage(message)
 
-    def onClose(self, wasClean, code, reason):
-        print("WebSocket connection closed: {0}".format(reason))
+    def connectionLost(self, reason):
+        WebSocketServerProtocol.connectionLost(self, reason)
+        self.factory.unregister(self)
 
+
+class BroadcastServerFactory(WebSocketServerFactory):
+
+    """
+    Simple broadcast server broadcasting any message it receives to all
+    currently connected clients.
+    """
+
+    def __init__(self, url, userStore):
+        WebSocketServerFactory.__init__(self, url)
+        self.userStore = userStore
+        self.clients = []
+        self.tickcount = 0
+        self.tick()
+
+    def tick(self):
+        self.tickcount += 1
+        # self.broadcast({
+        #     "message": "tick %d from server" % self.tickcount,
+        #     "origin": None
+        #     })
+        reactor.callLater(1, self.tick)
+
+    def register(self, client):
+        if client not in self.clients:
+            print("registered client {}".format(client.peer))
+            self.clients.append(client)
+
+    def unregister(self, client):
+        if client in self.clients:
+            print("unregistered client {}".format(client.peer))
+            self.clients.remove(client)
+
+    def broadcast(self, msg):
+        # print("broadcasting message '{}' ..".format(msg))
+        for c in self.clients:
+            # if(c.peer == msg["origin"]):
+            #     print("Skipping this entry because it's the origin")
+            #     continue
+            if c.peer in self.userStore.validatedPeers:
+                message = json.dumps({
+                    "type": "update",
+                    "data": msg["message"]
+                }).encode('utf8')
+                c.sendMessage(message)                
+                print("message sent to {}".format(c.peer))
+            else:
+                print "I know this peer but I don't know who they are"
+
+
+class BroadcastPreparedServerFactory(BroadcastServerFactory):
+
+    """
+    Functionally same as above, but optimized broadcast using
+    prepareMessage and sendPreparedMessage.
+    """
+
+    def broadcast(self, msg, userStore):
+        self.userStore = userStore
+        # print("broadcasting prepared message '{}' ..".format(msg))
+        preparedMsg = self.prepareMessage(msg)
+        for c in self.clients:
+            if c.peer in userStore.validatedPeers:
+                c.sendPreparedMessage(preparedMsg)
+            # print("prepared message sent to {}".format(c.peer))
 
 if __name__ == '__main__':
 
-    import sys
-
-    from twisted.python import log
-    from twisted.internet import reactor
-
     log.startLogging(sys.stdout)
 
-    factory = WebSocketServerFactory(u"ws://127.0.0.1:9000")
-    factory.protocol = MyServerProtocol
-    # factory.setProtocolOptions(maxConnections=2)
+    ServerFactory = BroadcastServerFactory
+    # ServerFactory = BroadcastPreparedServerFactory
 
-    # note to self: if using putChild, the child must be bytes...
 
-    reactor.listenTCP(9000, factory)
+    factory = ServerFactory(u"ws://127.0.0.1:9000", store)
+    factory.protocol = BroadcastServerProtocol
+    listenWS(factory)
+
+    webdir = File(".")
+    web = Site(webdir)
+    reactor.listenTCP(8080, web)
+
     reactor.run()
